f205e7272922198f0a3ad764e0712e106c2ac7cc
Noemi1 <noemi.almeida@aluno.faculdadeimpacta.com.br>
2021-05-19 13:35:06 -0300

    first commit

---
+++ Aluno/Aluno/Aluno.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aluno
+{
+    class Aluno
+    {
+
+        public Aluno()
+        {
+
+        }
+
+        public Aluno(int RA, string Nome, double Nota1, double Nota2)
+        {
+            this.RA = RA;
+            this.Nome = Nome;
+            this.Nota1 = Nota1;
+            this.Nota2 = Nota2;
+        }
+
+        public int RA
+        {
+            get { return RA; }
+            set
+            {
+                if (value > 0)
+                {
+                    RA = value;
+                }
+                else
+                {
+                    RA = 0;
+                }
+            }
+        }
+        public string Nome
+        {
+            get { return Nome; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Nome = "";
+                }
+                else if (value.Length > 5)
+                {
+
+                    Nome = "";
+                }
+                else
+                {
+                    Nome = value;
+                }
+            }
+        }
+        public double Nota1 { 
+            get { return Nota1;  } 
+            set
+            {
+                if ( value < 0)
+                {
+                    Nota1 = 0;
+                } 
+                else
+                {
+                    Nota1 = value;
+                }
+            }
+        }
+        public double Nota2
+        {
+            get { return Nota2; }
+            set
+            {
+                if (value < 0)
+                {
+                    Nota2 = 0;
+                }
+                else
+                {
+                    Nota2 = value;
+                }
+            }        
+        }
+
+        public double CalcularMedia()
+        {
+            return (this.Nota1 + this.Nota2) / 2;
+        }
+
+
+        public void VerificaSituacao()
+        {
+            double Media = CalcularMedia();
+            if (Media >= 5)
+                Console.WriteLine("Aprovado");
+            else
+                Console.WriteLine("Reprovado");
+        }
+
+
+
+    }
+}
---
+++ Aluno/Aluno/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aluno aluno = new Aluno();
+
+            //Console.Write("Nome do aluno: ");
+            //aluno.Nome = Console.ReadLine();
+            //Console.Write("RA: ");
+            //aluno.RA = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("NOTA 1: ");
+            //aluno.Nota1 = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("NOTA 2: ");
+            //aluno.Nota2 = Convert.ToDouble(Console.ReadLine());
+
+
+            //aluno.Nome = "Noemi";
+            //aluno.RA = 15;
+            //aluno.Nota1 = 10;
+            //aluno.Nota2 = 10;
+
+
+            Aluno aluno = new Aluno(125, "Noemi", 10, 10);
+            aluno.VerificaSituacao();
+
+
+
+
+        }
+    }
+}
